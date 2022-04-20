29b069844add6006746ca3e7abcf00664c9d0ae3
Anky <nasaankit01@gmail.com>
2022-04-20 12:12:41 +0530

    Updated image resize tool

--- Simpson_cnn/resize.py
+++ Simpson_cnn/resize.py
@@ -1,17 +1,32 @@
 import os
 from PIL import Image
+import sys
+from tqdm import tqdm
+f = sys.argv
 
-folder1 = r"/home/sacreds/Downloads/lenses/train/sub"
+# 0 - Filename
+# 1 - Image-size-x
+# 2 - Image-size-y
+# 3 - Parent folder
+# 4 - target directory
+if len(f) > 1:
+    s_x = int(f[1])
+    s_y = int(f[2])
 
-files = os.listdir(folder1)
+    folder1 = f[3]
+    folder2 = f[4]
+    files = os.listdir(folder1)
 
-size = (64,64)
-a = 0
+    size = (s_x,s_y)
+    a = 0
 
-for x in files:
-    a +=1
-    img = Image.open("{}/{}".format(folder1,x))
-    print("Resizing:",a)
-    r_img = img.resize(size)
-    print("Saving:",a)
-    r_img.save(r"/home/sacreds/Documents/GitHub/ML4SC_GSOC/Exploring Transformers/lenses/train/sub/{}.jpg".format(a))
+    for x in tqdm(files):
+        a +=1
+        img = Image.open("{}/{}".format(folder1,x))
+        
+        r_img = img.resize(size)
+        
+        r_img.save(r"{}/{}.jpg".format(folder2,a))
+    print("Completed")
+else:
+    print("Please provide arguments.\n image_x,image_y,paretntpath,targetpath")
